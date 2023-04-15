0580c67f6d141d1778ddec11980c14e62e6360e8
dammynex <damilolaofficial@gmail.com>
2023-04-15 20:55:38 +0100

    initial commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,64 +1,26 @@
 <script lang="ts" setup>
-import {
-  ref,
-  computed,
-  onMounted,
-  onUnmounted,
-  $navigateTo,
-} from "nativescript-vue";
-import Details from "./Details.vue";
+import { onMounted, ref, watch } from "nativescript-vue";
+import { setTimeout } from "@nativescript/core/timer";
 
-const counter = ref(0);
-const message = computed(() => {
-  return `Blank {N}-Vue app: ${counter.value}`;
-});
+const text = ref("");
+const tf = ref(null);
 
-function logMessage() {
-  console.log("You have tapped the message!");
-}
-
-let interval: any;
 onMounted(() => {
-  console.log("mounted");
-  interval = setInterval(() => counter.value++, 100);
-});
-
-onUnmounted(() => {
-  console.log("unmounted");
-  clearInterval(interval);
+  watch(text, (val) => console.log(val));
 });
 </script>
 
 <template>
-  <Frame>
-    <Page>
-      <ActionBar>
-        <Label text="Home" class="font-bold text-lg" />
-      </ActionBar>
-
-      <GridLayout rows="*, auto, auto, *" class="px-4">
-        <Label
-          row="1"
-          class="text-xl align-middle text-center text-gray-500"
-          :text="message"
-          @tap="logMessage"
-        />
-
-        <Button
-          row="2"
-          @tap="$navigateTo(Details)"
-          class="mt-4 px-4 py-2 bg-white border-2 border-blue-400 rounded-lg"
-          horizontalAlignment="center"
-        >
-          View Details
-        </Button>
-      </GridLayout>
-    </Page>
-  </Frame>
+  <Page>
+    <StackLayout>
+      <Label :text="text" textWrap="true" />
+      <TextField
+        class="p-5 bg-gray-100"
+        ref="tf"
+        hint=""
+        :text="text"
+        @textChange="($event) => (text = $event.object.text)"
+      />
+    </StackLayout>
+  </Page>
 </template>
-
-<style>
-/* .info {
-    font-size: 20;
-  } */
-</style>
